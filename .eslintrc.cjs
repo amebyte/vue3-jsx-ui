409c32c55315ddd3238dc9d6045a2348f187f27b
unknown <416212316@qq.com>
2022-12-03 22:25:56 +0800

    保存自动修复

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    // "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  // "parser": "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        // "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:vue/vue3-recommended",
-        "plugin:prettier/recommended"
-    ],
-    "overrides": [
-    ],
-    // "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module",
-        "ecmaFeatures":{
-            "jsx": true
-        }
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    "no-unused-vars": "off",
+    "no-self-assign": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-this-alias": ["off"],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    semi: 0,
+  },
+};
