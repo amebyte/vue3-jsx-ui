19451bc4b0d4d755587eeaf1eb199f92197d4aff
unknown <416212316@qq.com>
2022-12-03 21:18:19 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,3 @@
-# vue3-jsx-ui
-
-yarn create vite
-
-yarn add @vitejs/plugin-vue-jsx -D
-
-在 vite.config.ts 中导入 @vitejs/plugin-vue-jsx
-
-```
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -15,5 +6,3 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 export default defineConfig({
   plugins: [vue(), vueJsx()]
 })
-```
-
